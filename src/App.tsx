8745b11542f30d0dfb49b60a4492b5174a7bb605
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-11-13 12:13:12 -0600

    haciendo dispatch de createUser para guardar user en redux state global

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { DashboardPage } from './pages/dashboard';
 import { LoginPage } from './pages/login';
+import appStore from './redux/store';
 import './App.css';
 
 function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LoginPage />} />
-          <Route path='*' element={<h1>Not Found Page</h1>} />
-          <Route path='/dashboard' element={<DashboardPage />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<LoginPage />} />
+            <Route path='*' element={<h1>Not Found Page</h1>} />
+            <Route path='/dashboard' element={<DashboardPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   )
 }
--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,4 +1,24 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { createUser, resetUser } from '../../redux/states/userState';
+import { getMorty } from "../../services/authService";
+
 const LoginPage = () => {
+  const dispatch = useDispatch();
+
+  const _getMorty = async () => {
+    try {
+      const morty = await getMorty();
+      dispatch(createUser(morty));
+    } catch (error) {
+      dispatch(resetUser());
+    }
+  };
+
+  useEffect(() => {
+    _getMorty();
+  }, []);
+
   return (
     <div>
       <h1>Login</h1>
