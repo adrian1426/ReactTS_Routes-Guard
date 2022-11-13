14336cfa8fbeaf026b11501ba7b372880c1b4887
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-11-12 21:15:39 -0600

    Agregando nuestras rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { DashboardPage } from './pages/dashboard';
 import { LoginPage } from './pages/login';
+import './App.css';
 
 function App() {
 
   return (
     <div className="App">
-      <LoginPage />
-      <DashboardPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path='*' element={<h1>Not Found Page</h1>} />
+          <Route path='/dashboard' element={<DashboardPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
